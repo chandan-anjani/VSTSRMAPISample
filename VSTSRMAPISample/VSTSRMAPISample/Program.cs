9f5849e4a2affc91171799c9a3ac3f45d3a1b55d
Anjani Chandan <anjanich@microsoft.com>
2017-04-04 14:26:03 +0530

    Added few methods

--- VSTSRMAPISample/VSTSRMAPISample/Program.cs
+++ VSTSRMAPISample/VSTSRMAPISample/Program.cs
@@ -11,7 +11,7 @@ namespace VSTSRMAPISample
     {
         public static string vstsUrl = "https://anjani1.visualstudio.com";
         public static string projectName = "RMAPISample";
-
+        public static int releaseDefinitionId = 1;
 
         static void Main(string[] args)
         {
@@ -22,8 +22,33 @@ namespace VSTSRMAPISample
             VssConnection connection = new VssConnection(serverUrl, credentials);
 
             ReleaseHttpClient rmClient = connection.GetClient<ReleaseHttpClient>();
+
+            var releaseDefinition = GetReleaseDefintion(rmClient, releaseDefinitionId);
+
+            var release = CreateRelease(rmClient, releaseDefinition);
+
+            var releaseEnvironment = DeployAnEnvironment(rmClient, release.Id, release.Environments[3].Id);
         }
 
+       static ReleaseDefinition GetReleaseDefintion(ReleaseHttpClient rmClient, int releaseDefintionId)
+        {
+          return rmClient.GetReleaseDefinitionAsync(projectName, releaseDefintionId).Result;
+        }
 
+        static Release CreateRelease(ReleaseHttpClient rmClient, ReleaseDefinition releaseDefinition)
+        {
+            ReleaseStartMetadata rmMetaData = new ReleaseStartMetadata();
+            rmMetaData.DefinitionId = releaseDefinition.Id;
+
+            return rmClient.CreateReleaseAsync(rmMetaData, project: projectName).Result;
+        }
+
+        static ReleaseEnvironment DeployAnEnvironment(ReleaseHttpClient rmClient, int releaseId, int environmentIdToDeploy)
+        {
+            ReleaseEnvironmentUpdateMetadata envMetaData = new ReleaseEnvironmentUpdateMetadata();
+            envMetaData.Status = EnvironmentStatus.InProgress;
+            envMetaData.Comment = "Good to go";
+            return rmClient.UpdateReleaseEnvironmentAsync(envMetaData, projectName, releaseId, environmentIdToDeploy).Result;
+        }
     }
 }
