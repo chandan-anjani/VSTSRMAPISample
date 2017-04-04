b83ff050569d0196b6b92e5bc817e80b533f3557
Anjani Chandan <anjanich@microsoft.com>
2017-04-04 14:02:14 +0530

    Added a blank project

---
+++ VSTSRMAPISample/VSTSRMAPISample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.ReleaseManagement.WebApi;
+using Microsoft.VisualStudio.Services.ReleaseManagement.WebApi.Clients;
+using Microsoft.VisualStudio.Services.WebApi;
+
+namespace VSTSRMAPISample
+{
+    class Program
+    {
+        public static string vstsUrl = "https://anjani1.visualstudio.com";
+        public static string projectName = "RMAPISample";
+
+
+        static void Main(string[] args)
+        {
+            Uri serverUrl = new Uri(vstsUrl);
+            VssCredentials credentials = new VssClientCredentials();
+            credentials.Storage = new VssClientCredentialStorage();
+
+            VssConnection connection = new VssConnection(serverUrl, credentials);
+
+            ReleaseHttpClient rmClient = connection.GetClient<ReleaseHttpClient>();
+        }
+
+
+    }
+}
